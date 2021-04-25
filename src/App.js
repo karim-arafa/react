1cbabc8fc695206e680753953b334300fe2d4fe9
karim-arafa <karimahmedarafa@gmail.com>
2021-04-25 20:01:35 +0200

    lab1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+class App extends React.Component {
+  
+  constructor(){
+    super();
+    this.state={
+      initCount:5
+    }
+  }
+ 
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div> <Counter initCount={this.state.initCount} /> </div>
   );
 }
+}
 
+class Counter extends React.Component{
+
+  constructor(props){
+    super();
+    this.state = {
+      count:props.initCount
+    }
+  }
+  changeCount=(op)=>{
+    if(op=="+")
+      this.setState({count:this.state.count+1});
+    else if(op=="-")
+      this.setState({count:this.state.count-1});
+
+  }
+
+  render(){
+    
+    return (
+      <div>
+      <div>{this.state.count}</div>
+      <button onClick={()=>this.changeCount("+")} >Increase</button>
+      <button onClick={()=>this.changeCount("-")} >Decrease</button>
+      </div>
+    )
+}
+}
 export default App;
